c1d1b31257dc6461a9faadc49f9f51258c26f4f5
yoonki-kim <yoonki.kim71@gmail.com>
2021-10-11 13:25:06 +0900

    2021-10-11; install vuetify

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <v-btn>abcd</v-btn>
     </div>
     <router-view/>
   </div>
